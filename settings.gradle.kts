3f59dcaac1c3b8fe2cd38b4600a2a0a476c14842
khalidzeroual2 <98434519+khalidzeroual2@users.noreply.github.com>
2022-04-19 00:03:53 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
 rootProject.name = "GradleDemo"
+println("In the settings.gradle.kts file - This is executable during initialisation phase")
+println("In the settings,gradle.kts file - Determine which project take part in the build")
+include("App")
 
+plugins {
+  id("com.gradle.enterprise") version("3.9")
+  }
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
