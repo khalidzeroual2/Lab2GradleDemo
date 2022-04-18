e5a78e1fe46dcb494ffbbcbd6c107c994940218e
khalidzeroual2 <98434519+khalidzeroual2@users.noreply.github.com>
2022-04-18 23:58:11 +0100

    Add files via upload

---
+++ App/src/main/java/GradleDemo.java
@@ -0,0 +1,6 @@
+public class GradleDemo {
+
+    public static  void main(String [] args){
+        System.out.println("My Gradle Demo");
+    }
+}
---
+++ App/src/test/java/TestClassGradleDemo.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestClassGradleDemo {
+    @Test
+    public void testGradleDemo(){
+ System.out.println("test my Gradle Demo");
+}
+}
