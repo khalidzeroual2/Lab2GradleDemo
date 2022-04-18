2a8324c9c6cc23430aac4a2e8840827515ac163f
khalidzeroual2 <98434519+khalidzeroual2@users.noreply.github.com>
2022-04-18 23:59:11 +0100

    Delete src directory

--- src/main/java/GradleDemo.java
+++
@@ -1,6 +0,0 @@
-public class GradleDemo {
-
-    public static  void main(String [] args){
-        System.out.println("My Gradle Demo");
-    }
-}
--- src/test/java/TestClassGradleDemo.java
+++
@@ -1,8 +0,0 @@
-import org.testng.annotations.Test;
-
-public class TestClassGradleDemo {
-    @Test
-    public void testGradleDemo(){
- System.out.println("test my Gradle Demo");
-}
-}
